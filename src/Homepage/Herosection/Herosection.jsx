6fc218029568ab101015833c6da53afe0a28fddc
Excel Ikenna <ikennaexcel@gmail.com>
2025-01-30 09:49:30 +0100

    inor changes

--- src/Homepage/Herosection/Herosection.jsx
+++ src/Homepage/Herosection/Herosection.jsx
@@ -8,7 +8,7 @@ const Herosection = () => {
       <div className="text-center py-7 px-7 mb-7">
         <h1 className="tracking-tighter pb-7 text-6xl font-extrabold slide-in-elliptic-right-fwd">Hub of <span className="text-custompurple">GiftCards</span> & <span className="text-custompurple">Digital Payment</span> Solutions</h1>
         <p className="pb-7 slide-in-elliptic-left-fwd">Millicent is the best platform to buy and sell gift cards, pay utility bills and at the best rates too.</p>
-        <Link to = "/signup" >
+        <Link to = "/signin" >
         <button className="millicent-btn bg-customgreen text-white">Lets Trade</button>
         </Link>
       </div>
--- src/Pages/SignUpPage/SignIn.jsx
+++ src/Pages/SignUpPage/SignIn.jsx
@@ -15,7 +15,9 @@ const SignIn = () => {
       <form action="" className='flex flex-col gap-4 mt-7'>
             <input className='h-15 bg-customGray w-full p-5 rounded-xl' type="email" placeholder='Email' />
             <input className='h-15 bg-customGray w-full p-5 rounded-xl' type="password" placeholder='Password' />
-            <input className='h-15 bg-customGray w-full p-5 rounded-xl' type="password" placeholder='Confirm Password' />
+            <Link to = "/">
+            <p className='text-blue-500 font-bold'>Forgot Password ?</p>
+            </Link>
             <button className='h-15 p-5 text-white font-bold bg-custompurple rounded-xl'>Login</button>
         </form>
 
